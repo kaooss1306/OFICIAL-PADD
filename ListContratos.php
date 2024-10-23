4dcd34aa9f80e73e4b111c593a022fd78a9117cc
DEVMTW <hola@mejorandotuweb.com>
2024-10-23 19:18:45 -0300

    tu mensaje desde mi rama 22

--- ListContratos.php
+++ ListContratos.php
@@ -2,20 +2,16 @@
 // Iniciar sesión
 session_start();
 
-include 'querys/qcontratos.php';
-include 'componentes/header.php';
-include 'componentes/sidebar.php';
-
-
-
-
+include "querys/qcontratos.php";
+include "componentes/header.php";
+include "componentes/sidebar.php";
 ?>
 <!-- Main Content -->
       <div class="main-content">
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?php echo $ruta; ?>dashboard.php">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Lista de Contratos</li>
+            <li class="breadcrumb-item active" aria-current="page">Lista de Contratos2</li>
         </ol>
     </nav><br>
         <section class="section">
@@ -26,9 +22,9 @@ include 'componentes/sidebar.php';
                 <div class="card-header milinea">
                             <div class="titulox"><h4>Listado de Contratos</h4></div>
                             <div class="agregar">
-                            <a href="#" 
-       class="btn btn-primary open-modal" 
-       data-bs-toggle="modal" 
+                            <a href="#"
+       class="btn btn-primary open-modal"
+       data-bs-toggle="modal"
        data-bs-target="#modalAddContrato">
         <i class="fas fa-plus-circle"></i> Agregar Contrato
     </a>
@@ -53,43 +49,85 @@ include 'componentes/sidebar.php';
                           </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($contratos as $contrato): 
-                        $nombreCliente = isset($clientesMap[$contrato['IdCliente']]) ? $clientesMap[$contrato['IdCliente']]['nombreCliente'] : 'N/A';
-                        $nombreProducto = 'N/A';
-                        if (isset($productosMap[$contrato['IdCliente']]) && !empty($productosMap[$contrato['IdCliente']])) {
-                            $nombreProducto = $productosMap[$contrato['IdCliente']][0]['NombreDelProducto'];
-                        }
-                        $nombreProvee = isset($proveedorMap[$contrato['IdProveedor']]) ? $proveedorMap[$contrato['IdProveedor']]['nombreProveedor'] : 'N/A';
-                        $nombreMedio = isset($mediosMap[$contrato['IdMedios']]) ? $mediosMap[$contrato['IdMedios']]['NombredelMedio'] : 'N/A';
-                        $pagoForma = isset($pagosMap[$contrato['id_FormadePago']]) ? $pagosMap[$contrato['id_FormadePago']]['NombreFormadePago'] : 'N/A';
-                        ?>
+                        <?php foreach ($contratos as $contrato):
+
+                            $nombreCliente = isset(
+                                $clientesMap[$contrato["IdCliente"]]
+                            )
+                                ? $clientesMap[$contrato["IdCliente"]][
+                                    "nombreCliente"
+                                ]
+                                : "N/A";
+                            $nombreProducto = "N/A";
+                            if (
+                                isset($productosMap[$contrato["IdCliente"]]) &&
+                                !empty($productosMap[$contrato["IdCliente"]])
+                            ) {
+                                $nombreProducto =
+                                    $productosMap[$contrato["IdCliente"]][0][
+                                        "NombreDelProducto"
+                                    ];
+                            }
+                            $nombreProvee = isset(
+                                $proveedorMap[$contrato["IdProveedor"]]
+                            )
+                                ? $proveedorMap[$contrato["IdProveedor"]][
+                                    "nombreProveedor"
+                                ]
+                                : "N/A";
+                            $nombreMedio = isset(
+                                $mediosMap[$contrato["IdMedios"]]
+                            )
+                                ? $mediosMap[$contrato["IdMedios"]][
+                                    "NombredelMedio"
+                                ]
+                                : "N/A";
+                            $pagoForma = isset(
+                                $pagosMap[$contrato["id_FormadePago"]]
+                            )
+                                ? $pagosMap[$contrato["id_FormadePago"]][
+                                    "NombreFormadePago"
+                                ]
+                                : "N/A";
+                            ?>
                         <tr>
-    <td><?php echo $contrato['id']; ?></td>
-    <td><?php echo $contrato['NombreContrato']; ?></td>
+    <td><?php echo $contrato["id"]; ?></td>
+    <td><?php echo $contrato["NombreContrato"]; ?></td>
     <td><?php echo $nombreCliente; ?></td>
     <td><?php echo $nombreProducto; ?></td>
     <td><?php echo $nombreProvee; ?></td>
     <td><?php echo $nombreMedio; ?></td>
     <td><?php echo $pagoForma; ?></td>
     <td><div class="alineado">
-       <label class="custom-switch sino" data-toggle="tooltip" 
-       title="<?php echo $contrato['Estado'] ? 'Desactivar Contrato' : 'Activar Contrato'; ?>">
-    <input type="checkbox" 
+       <label class="custom-switch sino" data-toggle="tooltip"
+       title="<?php echo $contrato["Estado"]
+           ? "Desactivar Contrato"
+           : "Activar Contrato"; ?>">
+    <input type="checkbox"
            class="custom-switch-input estado-switchC"
-           data-id="<?php echo $contrato['id']; ?>" data-tipo="contrato" <?php echo $contrato['Estado'] ? 'checked' : ''; ?>> <span class="custom-switch-indicator"></span>
+           data-id="<?php echo $contrato[
+               "id"
+           ]; ?>" data-tipo="contrato" <?php echo $contrato["Estado"]
+    ? "checked"
+    : ""; ?>> <span class="custom-switch-indicator"></span>
 </label>
     </div></td>
     <td>
-      <a class="btn btn-primary micono" href="views/viewContrato.php?id=<?php echo $contrato['id']; ?>" data-toggle="tooltip" title="Ver Contrato"><i class="fas fa-eye "></i></a>
-      <button type="button" class="btn btn-success micono" data-bs-toggle="modal" data-bs-target="#modalEditContrato" data-contrato-id="<?php echo $contrato['id']; ?>"><i class="fas fa-pencil-alt"></i></button>
+      <a class="btn btn-primary micono" href="views/viewContrato.php?id=<?php echo $contrato[
+          "id"
+      ]; ?>" data-toggle="tooltip" title="Ver Contrato"><i class="fas fa-eye "></i></a>
+      <button type="button" class="btn btn-success micono" data-bs-toggle="modal" data-bs-target="#modalEditContrato" data-contrato-id="<?php echo $contrato[
+          "id"
+      ]; ?>"><i class="fas fa-pencil-alt"></i></button>
 
 
-                                            
 
-                                        
+
+
     </td>
 </tr>
-<?php endforeach; ?>
+<?php
+                        endforeach; ?>
                         </tbody>
                       </table>
                     </div>
@@ -100,13 +138,12 @@ include 'componentes/sidebar.php';
 
           </div>
         </section>
-        
+
       </div>
 
 <script src="assets/js/toggleContratos.js"></script>
 
-      <?php include 'componentes/settings.php'; ?>
-      <?php include 'querys/modulos/modalAddContrato.php'; ?>
-      <?php include 'querys/modulos/modalEditContrato.php'; ?>
-      <?php include 'componentes/footer.php'; ?>
-      
+      <?php include "componentes/settings.php"; ?>
+      <?php include "querys/modulos/modalAddContrato.php"; ?>
+      <?php include "querys/modulos/modalEditContrato.php"; ?>
+      <?php include "componentes/footer.php"; ?>
